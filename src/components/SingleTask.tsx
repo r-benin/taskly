caf225c210fe644803095bf60440245ac03fbf94
RBenin <rdbenin@gmail.com>
2024-02-03 21:49:01 +0800

    Add button functions

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import { Task } from './Model';
 import { FiEdit } from "react-icons/fi";
-import { MdCheckBox } from "react-icons/md";
 import { FiCheck } from "react-icons/fi";
 import { FiTrash2 } from "react-icons/fi";
 
@@ -15,16 +14,72 @@ type Props = {
 }
 
 const SingleTask = ({singleTask, tasks, setTask}: Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTask, setEditTask] = useState<string>(singleTask.task);
+    
+    const handleEdit = (e:React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTask(
+            tasks.map((task) =>
+            task.id === id ? { ...task, task: editTask }: task)
+        )
+        setEdit(false); 
+    };
+
+    const handleDone = (id: number) => {
+        setTask(tasks.map((task) =>
+            task.id === id ? { ...task, isDone: !task.isDone } : task
+        ))
+    };
+
+    const handleDelete = (id: number) => {
+        setTask(tasks.filter((task)=> task.id !== id))
+    };
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(()=> {
+        inputRef.current?.focus();
+    }, [edit])
+
     return (
-        <form className="task">
-            <div className="task-buttons">
-                <span className="icon"><FiEdit /></span>
-                <span className="icon"><FiCheck /></span>
-                <span className="icon"><FiTrash2 /></span>
-            </div>
-            <div className="task-content">{singleTask.task}</div>
+        singleTask.isDone ? (
+            <form className="done-task">
+                <div className="task-buttons">
+                    <span className="icon"><FiEdit /></span>
+                    <span className="icon" onClick={() => handleDone(singleTask.id)}><FiCheck /></span>
+                    <span className="icon" onClick={() => handleDelete(singleTask.id)}><FiTrash2 /></span>
+                </div>
+                {
+                    singleTask.isDone ? (
+                        <div className="task-content"><s>{singleTask.task}</s></div>
+                    ) : (
+                        <div className="task-content">{singleTask.task}</div>
+                    )
+                }
         </form>
+        ) : (
+            <form className="task" onSubmit={(e) => handleEdit(e, singleTask.id)}>
+                <div className="task-buttons">
+                    <span className="icon" onClick={() => setEdit(!edit)}><FiEdit /></span>
+                    <span className="icon" onClick={() => handleDone(singleTask.id)}><FiCheck /></span>
+                    <span className="icon" onClick={() => handleDelete(singleTask.id)}><FiTrash2 /></span>
+                </div>
+                { 
+                    edit ? (
+                            <div className="edit-content">
+                                <input className="input-edit-content" value={editTask} ref={inputRef} onChange={(e) => setEditTask(e.target.value)}></input>
+                                <small className="edit-tooltip">Press "enter" to confirm edit</small>
+                            </div>
+                            
+                        ) : (
+                            <div className="task-content">{singleTask.task}</div>
+                        )
+                }
+            </form>
+        )
+            
+        
     )
 }
-
 export default SingleTask
